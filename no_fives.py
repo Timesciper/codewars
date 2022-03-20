15ac80b10a7c4c008936380ded2bda5e37f6fe0b
Squealer <timesciper@live.ru>
2022-03-20 06:05:32 +0300

    no fives

---
+++ no_fives.py
@@ -0,0 +1,9 @@
+"""
+https://www.codewars.com/kata/5813d19765d81c592200001a/train/python
+вернуть сколько чисел из промежутка не имеют число цифру 5 в себе
+первое число всеггда меньше второго, могут быть отрицательные промежутки
+"""
+
+
+def dont_give_me_five(start,end):
+    return len([x for x in range(start, end+1) if "5" not in list(str(x))])
