3b4ffb92b955ffb5daaf3a52654c2391e3eeafc3
Squealer <timesciper@live.ru>
2022-05-03 02:02:24 +0300

    no_five

--- no_fives.py
+++ no_fives.py
@@ -5,5 +5,5 @@ https://www.codewars.com/kata/5813d19765d81c592200001a/train/python
 """
 
 
-def dont_give_me_five(start,end):
+def dont_give_me_five(start, end):
     return len([x for x in range(start, end+1) if "5" not in list(str(x))])
